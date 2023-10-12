26b991ada360ad1c7c8ed4c11315e118176abd7e
ALitch0 <leathheal@gmail.com>
2023-10-11 20:38:11 -0400

    First commit, used react router to link everthing

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// shared components
+import Header from "./views/shared/Header";
+import Footer from "./views/shared/Footer";
+
+// page components
+import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import Services from "./views/Services";
+
+// dependencies
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      {/* jsx comment style */}
+      <Header />
+      <main>
+        {/* use classes from react-router-dom to set up the url mapping for each page */}
+         <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+         </BrowserRouter>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Contact.js
@@ -0,0 +1,13 @@
+function Contact(){
+    return(
+        <section className="container">
+            <h1>Contact</h1>
+            <p>This is the contact section</p>
+            <ul>
+                <li><a href="www.twitter.com">Twitter</a></li>
+                <li><a href="www.facebook.com">Facebook</a></li>
+                </ul>
+        </section>
+    )
+}
+export default Contact;
